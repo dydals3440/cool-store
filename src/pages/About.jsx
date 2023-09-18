048b547884b734e23fea340be70020ab27c70d6e
김용민 <dydals3440@gmail.com>
2023-09-19 02:13:59 +0900

    feat: About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,27 @@
 const About = () => {
-  return <h1 className='text-4'>About</h1>;
+  return (
+    <>
+      <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
+        <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+          We love
+        </h1>
+        <div className='stats bg-primary shadow'>
+          <div className='stat'>
+            <div className='stat-title text-primary-content text-4xl font-bold tracking-widest'>
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className='mt-6 text-lg leading-8 max-w-2xl mx-auto'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe iure
+        vitae praesentium obcaecati adipisci in amet tempora cum! Nostrum ullam
+        optio voluptatibus aperiam aut nesciunt nihil, cumque tempora culpa,
+        dolores non explicabo distinctio laudantium eum animi recusandae earum
+        minus rem.
+      </p>
+    </>
+  );
 };
 
 export default About;
