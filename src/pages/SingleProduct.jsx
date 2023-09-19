4bb653af7c17c4d5b0c50bcef31dbfd6cdef1071
김용민 <dydals3440@gmail.com>
2023-09-19 14:12:06 +0900

    feat: fetch single product data using useLoaderData

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,4 +1,19 @@
+import { useLoaderData } from 'react-router-dom';
+import { formatPrice, customFetch } from '../utils';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+export const loader = async ({ params }) => {
+  const response = await customFetch(`/products/${params.id}`);
+  const singleProduct = response.data.data;
+  return { product: singleProduct };
+};
+
 const SingleProduct = () => {
+  const { product } = useLoaderData();
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+  const wonAmount = formatPrice(price);
   return <div>SingleProduct</div>;
 };
 export default SingleProduct;
