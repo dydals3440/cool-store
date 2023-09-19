eda136deea7829472f16f05ff58f4fc97fd39c58
김용민 <dydals3440@gmail.com>
2023-09-19 14:00:53 +0900

    feat: formatting price hook

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,12 @@ const productionUrl = 'https://strapi-store-server.onrender.com/api';
 export const customFetch = axios.create({
   baseURL: productionUrl,
 });
+
+// Formatting Price
+export const formatPrice = (price) => {
+  const wonAmount = new Intl.NumberFormat('ko-KR', {
+    style: 'currency',
+    currency: 'KRW',
+  }).format((price / 100).toFixed(2));
+  return wonAmount;
+};
