56ce6ab566bb7cc7cf1846dab98dae8316100eaa
김용민 <dydals3440@gmail.com>
2023-09-19 22:34:51 +0900

    feat: search input react-router-dom

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,4 +1,26 @@
+import { Form, useLoaderData, Link } from 'react-router-dom';
+import FormInput from './FormInput';
+
 const Filters = () => {
-  return <div>Filters</div>;
+  return (
+    // Form의 default는 get요청
+    <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
+      {/* SEARCH */}
+      {/* name을 제대로 지정해줘야함 검색어 입력후, 서치버튼을 누르면 products?search=검색어로 쿼리파람 사용함 */}
+      <FormInput
+        type='search'
+        label='search product'
+        name='search'
+        size='input-sm'
+      />
+      {/* BUTTONS */}
+      <button type='submit' className='btn btn-primary btn-sm'>
+        search
+      </button>
+      <Link to='/products' className='btn btn-accent btn-sm'>
+        reset
+      </Link>
+    </Form>
+  );
 };
 export default Filters;
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,14 +1,15 @@
-const FormInput = ({ label, name, type, defaultValue }) => {
+const FormInput = ({ label, name, type, defaultValue, size }) => {
   return (
     <div className='form-control '>
       <label className='label'>
-        <span className='label-text'>{label}</span>
+        <span className='label-text capitalize'>{label}</span>
       </label>
       <input
         type={type}
         name={name}
         defaultValue={defaultValue}
-        className='input input-bordered '
+        // size prop으로 전달안할시 default: medium
+        className={`input input-bordered ${size}`}
       />
     </div>
   );
