155a2207f43ec65559ab07b68f49283e92d59fca
김용민 <dydals3440@gmail.com>
2023-09-21 01:38:18 +0900

    feat: pagination 구현

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,5 +1,68 @@
+import { useLoaderData, useLocation, useNavigate } from 'react-router-dom';
+
 const PaginationContainer = () => {
-  return <div>PaginationContainer</div>;
+  const { meta } = useLoaderData();
+  const { pageCount, page } = meta.pagination;
+
+  // Array.from
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1;
+  });
+
+  // search는 query params
+  // pathname은 쿼리파람 전 경로를 의미함
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set('page', pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+  // 페이지가 1페이지면, 컨테이너를 전부 전시할 필요가없음
+  if (pageCount < 2) return null;
+  return (
+    <div className='mt-16 flex justify-end'>
+      <div className='join'>
+        <button
+          className='btn btn-xs sm:btn-md join-item'
+          onClick={() => {
+            let prevPage = page - 1;
+            if (prevPage < 1) prevPage = pageCount;
+            handlePageChange(prevPage);
+          }}
+        >
+          prev
+        </button>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              key={pageNumber}
+              onClick={() => {
+                handlePageChange(pageNumber);
+              }}
+              className={`btn btn-xs sm:btn-md border-none join-item ${
+                pageNumber === page ? 'bg-base-300 border-base-300' : ''
+              }`}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+        <button
+          className='btn btn-xs sm:btn-md join-item'
+          onClick={() => {
+            let nextPage = page + 1;
+            // 다음 페이지가, 최대 페이지보다 크면 안됨
+            if (nextPage > pageCount) nextPage = pageCount;
+            handlePageChange(nextPage);
+          }}
+        >
+          next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PaginationContainer;
