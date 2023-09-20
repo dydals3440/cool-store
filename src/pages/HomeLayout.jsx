02de988e580a76855232fc412efa3445b81cf2b1
김용민 <dydals3440@gmail.com>
2023-09-20 23:41:56 +0900

    feat: useNavigation 활용 Loading 상태 구현

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,16 +1,22 @@
-import { Outlet } from 'react-router-dom';
-import { Header, Navbar } from '../components';
+import { Outlet, useNavigation } from 'react-router-dom';
+import { Header, Navbar, Loading } from '../components';
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading';
   return (
     <>
       <nav>
         <Header />
         <Navbar />
+        {isPageLoading ? (
+          <Loading />
+        ) : (
+          <section className='align-element py-20'>
+            <Outlet />
+          </section>
+        )}
       </nav>
-      <section className='align-element py-20'>
-        <Outlet />
-      </section>
     </>
   );
 };
