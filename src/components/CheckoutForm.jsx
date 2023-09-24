e0544b23ab8203d1a76ee8e0f30dfc042ead5c08
김용민 <dydals3440@gmail.com>
2023-09-24 21:57:09 +0900

    feat: checkout page setup

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,5 +1,23 @@
+import { Form } from 'react-router-dom';
+import SubmitBtn from './SubmitBtn';
+import FormInput from './FormInput';
+
+export const action = (store) => async () => {
+  console.log(store.getState());
+  return null;
+};
+
 const CheckoutForm = () => {
-  return <h1 className='text-3xl'>CheckoutForm</h1>;
+  return (
+    <Form method='POST' className='flex flex-col gap-y-4'>
+      <h4 className='font-medium text-xl capitalize'>shipping information</h4>
+      <FormInput label='first name' name='name' type='text' />
+      <FormInput label='address' name='address' type='address' />
+      <div className='mt-4'>
+        <SubmitBtn text='place your order' />
+      </div>
+    </Form>
+  );
 };
 
 export default CheckoutForm;
