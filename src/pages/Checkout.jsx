131c4c939fdfe4c3583a0ec5aaf77fd2223c3acc
김용민 <dydals3440@gmail.com>
2023-09-24 20:44:24 +0900

    feat: Checkout Page Setup

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,20 @@
+import { useSelector } from 'react-redux';
+import { CheckoutForm, SectionTitle, CartTotals } from '../components';
+
 const CheckOut = () => {
-  return <h1 className='text-4'>CheckOut</h1>;
+  const cartTotal = useSelector((state) => state.cartState.cartTotal);
+  if (cartTotal === 0) {
+    return <SectionTitle text={'Your Cart is Empty'} />;
+  }
+  return (
+    <>
+      <SectionTitle text='place your order' />
+      <div className='mt-8 grid gap-8 md:grid-cols-2 items-start'>
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  );
 };
 
 export default CheckOut;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,6 +31,24 @@ export const action =
   };
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const loginAsGuestUser = async () => {
+    try {
+      const response = await customFetch.post('/auth/local', {
+        identifier: 'test@test.com',
+        password: 'secret',
+      });
+      dispatch(loginUser(response.data));
+      toast.success('welcome guest user');
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+      toast.error('guest user login error. please check out');
+    }
+  };
+
   return (
     <section className='h-screen grid place-items-center'>
       <Form
@@ -38,24 +56,18 @@ const Login = () => {
         className='card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4'
       >
         <h4 className='text-center text-3xl font-bold'>Login</h4>
-        <FormInput
-          type='email'
-          label='email'
-          name='identifier'
-          defaultValue='text@test.com'
-        />
-        <FormInput
-          type='password'
-          label='password'
-          name='password'
-          defaultValue='secret'
-        />
+        <FormInput type='email' label='email' name='identifier' />
+        <FormInput type='password' label='password' name='password' />
         <div className='mt-4'>
           {/* type: submit 별도의 요청 처리가 들어간 버튼 */}
           <SubmitBtn text='login' />
         </div>
         {/* 별도의 요청처리 x */}
-        <button type='button' className='btn btn-secondary btn-block'>
+        <button
+          type='button'
+          className='btn btn-secondary btn-block'
+          onClick={loginAsGuestUser}
+        >
           guest user
         </button>
         <p className='text-center'>
