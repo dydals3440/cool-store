271998707791ab5c69391bee3942df5e9303490a
김용민 <dydals3440@gmail.com>
2023-09-28 22:46:25 +0900

    feat: complex pagination

---
+++ src/components/ComplexPaginationContainer.jsx
@@ -0,0 +1,101 @@
+import { useLoaderData, useLocation, useNavigate } from 'react-router-dom';
+
+const ComplexPaginationContainer = () => {
+  const { meta } = useLoaderData();
+  const { pageCount, page } = meta.pagination;
+
+  // search는 query params
+  // pathname은 쿼리파람 전 경로를 의미함
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set('page', pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
+  const addPageButton = ({ pageNumber, activeClass }) => {
+    return (
+      <button
+        key={pageNumber}
+        onClick={() => {
+          handlePageChange(pageNumber);
+        }}
+        className={`btn btn-xs sm:btn-md border-none join-item ${
+          activeClass ? 'bg-base-300 border-base-300' : ''
+        }`}
+      >
+        {pageNumber}
+      </button>
+    );
+  };
+
+  const renderPageButtons = () => {
+    const pageButtons = [];
+    // first button
+    pageButtons.push(addPageButton({ pageNumber: 1, activeClass: page === 1 }));
+
+    // dots
+    if (page > 2) {
+      pageButtons.push(
+        <button className='join-item btn btn-xs sm:btn-md' key='dots-1'>
+          ...
+        </button>
+      );
+    }
+
+    // active / current page
+    if (page !== 1 && page !== pageCount) {
+      pageButtons.push(addPageButton({ pageNumber: page, activeClass: true }));
+    }
+
+    // dots
+    if (page < pageCount - 1) {
+      pageButtons.push(
+        <button className='join-item btn btn-xs sm:btn-md' key='dots-2'>
+          ...
+        </button>
+      );
+    }
+
+    // last button
+    pageButtons.push(
+      addPageButton({ pageNumber: pageCount, activeClass: page === pageCount })
+    );
+    return pageButtons;
+  };
+
+  // 페이지가 1페이지면, 컨테이너를 전부 전시할 필요가없음
+  if (pageCount < 2) return null;
+  return (
+    <div className='mt-16 flex justify-end'>
+      <div className='join'>
+        <button
+          className='btn btn-xs sm:btn-md join-item'
+          onClick={() => {
+            let prevPage = page - 1;
+            if (prevPage < 1) prevPage = pageCount;
+            handlePageChange(prevPage);
+          }}
+        >
+          prev
+        </button>
+        {renderPageButtons()}
+        <button
+          className='btn btn-xs sm:btn-md join-item'
+          onClick={() => {
+            let nextPage = page + 1;
+            // 다음 페이지가, 최대 페이지보다 크면 안됨
+            if (nextPage > pageCount) nextPage = pageCount;
+            handlePageChange(nextPage);
+          }}
+        >
+          next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ComplexPaginationContainer;
